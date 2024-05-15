b5f873103da239ee78ae1a815504f79259205ebc
Joshua Daniel David <2222551@slu.edu.ph>
2024-05-15 10:00:24 +0800

    organize dataset files

--- for_da_dataset/checktrans.py
+++ for_da_dataset/checktrans.py
@@ -3,14 +3,7 @@ from google.cloud import translate
 def get_supported_languages(
     project_id: str = "caramel-galaxy-423217-u2",
 ) -> list:
-    """Getting a list of supported language codes.
 
-    Args:
-        project_id: The GCP project ID.
-
-    Returns:
-        A list of supported language codes.
-    """
     client = translate.TranslationServiceClient()
 
     parent = f"projects/{project_id}"
